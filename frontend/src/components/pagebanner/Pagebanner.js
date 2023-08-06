cdda453c3eb2daf0d0c70237a22b9ca640bed5ed
Kavishka Chamudith <kavishkachamuditha95@gmail.com>
2023-08-06 22:35:58 +0530

    page banner still doing some updates

--- frontend/src/components/pagebanner/Pagebanner.js
+++ frontend/src/components/pagebanner/Pagebanner.js
@@ -2,35 +2,51 @@ import React from 'react'
 import Navpart from '../Navigation/Navpart'
 import './pagebanner.css'
 import Carousel from 'react-bootstrap/Carousel'
+import { Button } from 'react-bootstrap'
 
 function Pagebanner() {
   return (
     <div>
       <Navpart/>
-    
+     
 <Carousel>
   <Carousel.Item>
   <img className='imgstyle' src='images/wallpaperflare.com_wallpaper.jpg' alt=''/>
     <Carousel.Caption>
     <img className='bannerprtimg' src='images/1.jpg' alt=''/>
-      <h1 className='filmnme'>Avatar</h1>
-      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+      <h1 className='filmnme'  style={{ marginLeft: '-20px' }}>Avatar Two</h1>
+      <p className='filmintro'> A paraplegic Marine dispatched to the moon Pandora on a unique mission becomes <br/> <section className='filmintrosec'> torn between following his orders and protecting the world he feels is his home. </section></p>
+      <div className='button-container'>
+        <Button className='catgary'>Drama</Button>
+        <Button className='catgary'>Action</Button>
+        <p className='hourstxt'>R 3h 0m</p>
+      </div>
     </Carousel.Caption>
   </Carousel.Item>
   <Carousel.Item>
   <img className='imgstyle' src='images/wallpaperflare.com_wallpaper (1).jpg' alt=''/>
     <Carousel.Caption>
     <img className='bannerprtimg' src='images/2.jpg' alt=''/>
-      <h3>Second slide label</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <h1 className='filmnme'  style={{ marginLeft: '-90px' }}>Avengers</h1>
+      <p className='filmintro'> A paraplegic Marine dispatched to the moon Pandora on a unique mission becomes <br/> <section className='filmintrosec'> torn between following his orders and protecting the world he feels is his home. </section></p>
+      <div className='button-container'>
+        <Button className='catgary'>Drama</Button>
+        <Button className='catgary'>Action</Button>
+        <p className='hourstxt'>R 3h 0m</p>
+      </div>
     </Carousel.Caption>
   </Carousel.Item>
   <Carousel.Item>
   <img className='imgstyle' src='images/oppenheimer.jpg' alt=''/>
     <Carousel.Caption>
       <img className='bannerprtimg' src='images/FvUVt3hXgAAxP1H.jpeg' alt=''/>
-      <h3 className='text-light'>Third slide label</h3>
-      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+      <h1 className='filmnme' style={{ marginLeft: '50px' }}>Oppenheimer</h1>
+      <p className='filmintro'> A paraplegic Marine dispatched to the moon Pandora on a unique mission becomes <br/> <section className='filmintrosec'> torn between following his orders and protecting the world he feels is his home. </section></p>
+      <div className='button-container'>
+        <Button className='catgary'>Drama</Button>
+        <Button className='catgary'>Action</Button>
+        <p className='hourstxt'>R 3h 0m</p>
+      </div>
     </Carousel.Caption>
   </Carousel.Item>
 </Carousel>
