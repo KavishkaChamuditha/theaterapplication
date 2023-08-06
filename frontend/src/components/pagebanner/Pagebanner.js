a25cc9b102703ff5064ff93108bc01bab592cc4f
Kavishka Chamudith <kavishkachamuditha95@gmail.com>
2023-08-06 13:13:38 +0530

    banner still doing

--- frontend/src/components/pagebanner/Pagebanner.js
+++ frontend/src/components/pagebanner/Pagebanner.js
@@ -12,13 +12,15 @@ function Pagebanner() {
   <Carousel.Item>
   <img className='imgstyle' src='images/wallpaperflare.com_wallpaper.jpg' alt=''/>
     <Carousel.Caption>
-      <h3>First slide label</h3>
+    <img className='bannerprtimg' src='images/1.jpg' alt=''/>
+      <h1 className='filmnme'>Avatar</h1>
       <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
     </Carousel.Caption>
   </Carousel.Item>
   <Carousel.Item>
   <img className='imgstyle' src='images/wallpaperflare.com_wallpaper (1).jpg' alt=''/>
     <Carousel.Caption>
+    <img className='bannerprtimg' src='images/2.jpg' alt=''/>
       <h3>Second slide label</h3>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
     </Carousel.Caption>
@@ -26,6 +28,7 @@ function Pagebanner() {
   <Carousel.Item>
   <img className='imgstyle' src='images/oppenheimer.jpg' alt=''/>
     <Carousel.Caption>
+      <img className='bannerprtimg' src='images/FvUVt3hXgAAxP1H.jpeg' alt=''/>
       <h3 className='text-light'>Third slide label</h3>
       <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
     </Carousel.Caption>
